2b6c71bb3ef953a731f7b5f047937a25711c4474
pujithajanu <146074105+pujithajanu@users.noreply.github.com>
2024-07-24 09:57:22 +0530

    Add files via upload

---
+++ EVEN_INDEX_AND_ODD_INDEX.py
@@ -0,0 +1,10 @@
+#print the characters at even index and odd index
+st=input()
+print('characters of even index:')
+for i in range(,n):
+    print(st[i],end='')
+    i+=1
+print('charaters at odd index:')
+for i in range(1,n):
+    print(st[i],end='')
+    i+=1
